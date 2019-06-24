a53105ffcfdcdb46686429407f3db7b4daafbcce
theol0403 <theol0403@gmail.com>
2019-06-23 19:20:14 -0600

    upgrade

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,18 +1,25 @@
 #include "main.h"
 
 /**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
+* Runs the operator control code. This function will be started in its own task
+* with the default priority and stack size whenever the robot is enabled via
+* the Field Management System or the VEX Competition Switch in the operator
+* control mode.
+*
+* If no competition control is connected, this function will run immediately
+* following initialize().
+*
+* If the robot is disabled or communications is lost, the
+* operator control task will be stopped. Re-enabling the robot will restart the
+* task, not resume it from where it left off.
+*/
 void opcontrol() {
+
+ auto chassis = ChassisControllerBuilder()
+ .withMotors(-18, 19)
+ .withDimensions({{4_in, 11.5_in}, imev5GreenTPR})
+ .withMaxVelocity(1)
+ .build();
+ chassis->moveDistance(1_in);
 
 }
