94817abe567d809c85245b46100e04af5fde4f93
theol0403 <theol0403@gmail.com>
2019-07-07 01:11:12 -0600

    clean opcontrol and readme

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -19,15 +19,17 @@ void opcontrol() {
  auto chassis = ChassisControllerBuilder()
  .withMotors(1, -2)
  .withDimensions({{4_in, 11_in}, imev5RedTPR})
- .withMaxVelocity(50)
+ .withMaxVelocity(50) //so robot does not move too fast
  .withOdometry()
  .buildOdometry();
 
  OdomDebug display(lv_scr_act(), LV_COLOR_ORANGE, chassis);
 
  // chassis->moveDistance(1_ft);
- // chassis->turnAngle(4.0/11.0 * 360.0 * degree);
+ // chassis->turnAngle(90_deg);
+
  //arcade code goes here
- pros::delay(40000000);
+
+ pros::delay(40000000); //don't go out of scope
 
 }
